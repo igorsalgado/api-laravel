473f00556e9d0fe323f77def497e02f4995d2ca6
Igor Salgado <salgado.igor.is@gmail.com>
2024-04-25 21:05:43 -0300

    Feat: implementando update e delete

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\InvoiceResource;
+use App\Http\Resources\V1\UserResource;
 use App\Models\Invoice;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
@@ -30,7 +31,7 @@ class InvoiceController extends Controller
             'user_id' => 'required',
             'type' => 'required|max:1',
             'paid' => 'required|numeric|between:0,1',
-            'payment_date' => 'nullabe',
+            'payment_date' => 'nullable',
             'value' => 'required|numeric|between:1,9999.99',
         ]);
 
@@ -50,24 +51,55 @@ class InvoiceController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Invoice $invoice)
     {
-        //
+        return new InvoiceResource($invoice);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Invoice $invoice)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'type' => 'required|max:1|in:' . implode(',', ['B', 'C', 'P']),
+            'paid' => 'required|numeric|between:0,1',
+            'value' => 'required|numeric',
+            'payment_date' => 'date_format:Y-m-d H:i:s|nullable'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Validation failed', 422, $validator->errors());
+        }
+
+        $validated = $validator->validate();
+
+        $updated = $invoice->update([
+            'user_id' => $validated['user_id'],
+            'type' => $validated['type'],
+            'paid' => $validated['paid'],
+            'value' => $validated['value'],
+            'payment_date' => $validated['paid'] ? $validated['payment_date'] : null
+        ]);
+
+        if ($updated) {
+            return $this->response('Invoice updated', 200, new InvoiceResource($invoice->load('user')));
+        }
+
+        return $this->error('Invoice not updated', 400);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Invoice $invoice)
     {
-        //
+        $deleted = $invoice->delete();
+
+        if ($deleted) {
+            return $this->response('Invoice deleted', 200);
+        }
+        return $this->error('Invoice not deleted', 400);
     }
 }
