286e324ac79bd8a0229016426846e8dc3091019c
Igor Salgado <salgado.igor.is@gmail.com>
2024-05-05 15:50:11 -0300

    Trabalhando com filtros

--- app/Filters/Filter.php
+++ app/Filters/Filter.php
@@ -3,33 +3,64 @@
 namespace App\Filters;
 
 use DeepCopy\Exception\PropertyException;
+use Exception;
 use Illuminate\Http\Request;
 
-class Filter
+abstract class Filter
 {
-
     protected array $allowedOperatorsFields = [];
 
-    protected array $translateOperatorsFields = [
+    protected array $transalateOperatorsFields = [
         'gt' => '>',
         'gte' => '>=',
         'lt' => '<',
-        'lte' => '=<',
+        'lte' => '<=',
         'eq' => '=',
+        'ne' => '!=',
         'in' => 'in',
     ];
 
     public function filter(Request $request)
     {
-
         $where = [];
         $whereIn = [];
 
         if (empty($this->allowedOperatorsFields)) {
-            throw new PropertyException("Property allowedOperatorsFields is empty");
+            throw new PropertyException("Property allowedOperatorsfields is empty");
         }
 
         foreach ($this->allowedOperatorsFields as $param => $operators) {
+            $queryOperator = $request->query($param);
+            if ($queryOperator) {
+                foreach ($queryOperator as $operator => $value) {
+                    if (!in_array($operator, $operators)) {
+                        throw new Exception("{$param} does not have {$operator} operator");
+                    }
+
+                    if (str_contains($value, '[')) {
+                        $whereIn[] = [
+                            $param,
+                            explode(',', str_replace(['[', ']'], ['', ''], $value)),
+                            $value
+                        ];
+                    } else {
+                        $where[] = [
+                            $param,
+                            $this->transalateOperatorsFields[$operator],
+                            $value
+                        ];
+                    }
+                }
+            }
         }
+
+        if (empty($where) && empty($whereIn)) {
+            return [];
+        }
+
+        return [
+            'where' => $where,
+            'whereIn' => $whereIn
+        ];
     }
 }
